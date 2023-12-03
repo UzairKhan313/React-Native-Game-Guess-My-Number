78d9bded69e38d84f9921543c0bfba9538559d75
UzairKhan313 <uzairkhaan2003@gmail.com>
2023-12-03 14:35:40 +0000

    Styling Primarry Buttons

--- screens/GameStartScreen.js
+++ screens/GameStartScreen.js
@@ -10,8 +10,14 @@ function GameStartScreen() {
         keyboardType="number-pad"
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
@@ -19,6 +25,8 @@ module.exports = GameStartScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 100,
     padding: 16,
     marginHorizontal: 24,
@@ -42,4 +50,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: { flex: 1 },
 });
