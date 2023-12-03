70811713fc614d2a4ca032cbaff5746a2999a67a
UzairKhan313 <uzairkhaan2003@gmail.com>
2023-12-03 14:51:43 +0000

    Adding linear Gradient to the backgorund

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import GameStartScreen from "./screens/GameStartScreen";
 
 export default function App() {
-  return <GameStartScreen />;
+  return (
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <GameStartScreen />
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: { flex: 1 },
+});
