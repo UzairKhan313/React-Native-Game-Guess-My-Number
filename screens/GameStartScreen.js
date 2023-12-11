b9adce29481e9ddd68a24950753234ce07092984
UzairKhan313 <uzairkhaan2003@gmail.com>
2023-12-11 16:01:49 +0000

    Working with different size with orientation

--- screens/GameStartScreen.js
+++ screens/GameStartScreen.js
@@ -1,5 +1,15 @@
 import { useState } from "react";
-import { StyleSheet, View, TextInput, Alert, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Alert,
+  Text,
+  Dimensions,
+  useWindowDimensions,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
 import Title from "../Components/ui/Title";
 import PrimaryButton from "../Components/ui/PrimaryButton";
 import { Card } from "../Components/ui/Card";
@@ -7,6 +17,12 @@ import Colors from "../constants/colors";
 
 function GameStartScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
+
+  // Any code which interact with the screen orientation will kept in the component code.
+  // when the orientation is changed this components code wil be excuted.
+
+  // This hooks is used to get the heigth and width of the Screen when orientation is changed.
+  const { width, height } = useWindowDimensions();
 
   function inputNumberHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -25,40 +41,55 @@ function GameStartScreen({ onPickNumber }) {
       );
       return;
     }
-    console.log("Console Valid Number :  ", choosenNmber);
+
     onPickNumber(choosenNmber);
   }
+
+  const marginTopDistance = height < 380 ? 30 : 100;
   return (
-    <View style={styles.rootContainer}>
-      <Title>Guess My Number</Title>
-      <Card>
-        <Text style={styles.instructionText}>Enter a Number</Text>
-        <TextInput
-          style={styles.numberInput}
-          maxLength={2}
-          keyboardType="number-pad"
-          autoCorrect={false}
-          onChangeText={inputNumberHandler}
-          value={enteredNumber}
-        />
-        <View style={styles.buttonsContainer}>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-          </View>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={confrimInputHandler}>Confirm</PrimaryButton>
-          </View>
+    <ScrollView style={styles.keyboardScreen}>
+      <KeyboardAvoidingView style={styles.keyboardScreen} behavior="position">
+        <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
+          <Title>Guess My Number</Title>
+          <Card>
+            <Text style={styles.instructionText}>Enter a Number</Text>
+            <TextInput
+              style={styles.numberInput}
+              maxLength={2}
+              keyboardType="number-pad"
+              autoCorrect={false}
+              onChangeText={inputNumberHandler}
+              value={enteredNumber}
+            />
+            <View style={styles.buttonsContainer}>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+              </View>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={confrimInputHandler}>
+                  Confirm
+                </PrimaryButton>
+              </View>
+            </View>
+          </Card>
         </View>
-      </Card>
-    </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 }
 module.exports = GameStartScreen;
 
+// Window screen mean all screen without the status bar.
+// This code is excuted once when the app is started. it not excuted when the orientation is changed.
+// const devicHeigth = Dimensions.get("window").height
+
 const styles = StyleSheet.create({
+  keyboardScreen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
-    marginTop: 100,
+    // marginTop: devicHeigth < 380 ? 30 : 100,
     alignItems: "center",
   },
   instructionText: {
