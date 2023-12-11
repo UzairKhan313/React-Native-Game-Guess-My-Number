fd84e7d3408bc25baa51dfff0426d5de25b418f6
UzairKhan313 <uzairkhaan2003@gmail.com>
2023-12-11 16:39:47 +0000

    Style Game over Screen on Landscape mode

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,34 +1,65 @@
-import { StyleSheet, View, Text, Image, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  useWindowDimensions,
+  ScrollView,
+} from "react-native";
 
 import Title from "../Components/ui/Title";
 import Colors from "../constants/colors";
 import PrimaryButton from "../Components/ui/PrimaryButton";
 
-export const GameOverScreen = ({roundsNumber, userNumber, onStartNewGame}) => {
+export const GameOverScreen = ({
+  roundsNumber,
+  userNumber,
+  onStartNewGame,
+}) => {
+  const { width, height } = useWindowDimensions();
+
+  let imageSize = 300;
+  if (width < 380) {
+    imageSize = 150;
+  }
+  if (height  < 400) {
+    imageSize = 80;
+  }
+  const imageStyle = {
+    height: imageSize,
+    width: imageSize,
+    borderRadius: imageSize / 2,
+  };
   return (
-    <View style={styles.rootContainer}>
-      <Title>GAME OVER!</Title>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={require("../assets/images/success.png")}
-        />
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>GAME OVER!</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image
+            style={styles.image}
+            source={require("../assets/images/success.png")}
+          />
+        </View>
+        <View>
+          <Text style={styles.summeryText}>
+            Your Phone Needed{" "}
+            <Text style={styles.highlight}> {roundsNumber} </Text> Rounds To
+            Guess A Number
+            <Text style={styles.highlight}> {userNumber} </Text>
+          </Text>
+        </View>
+        <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
       </View>
-      <View>
-        <Text style={styles.summeryText}>
-          Your Phone Needed <Text style={styles.highlight}> {roundsNumber} </Text> Rounds To
-          Guess A Number
-          <Text style={styles.highlight}> {userNumber} </Text>
-        </Text>
-      </View>
-      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
-    </View>
+    </ScrollView>
   );
 };
 
-const devicWidth = Dimensions.get("window").width;
+// const devicWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     padding: 20,
@@ -36,9 +67,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   imageContainer: {
-    width: devicWidth < 380 ? 150 : 300,
-    height: devicWidth < 380 ? 150 : 300,
-    borderRadius: devicWidth < 380 ? 75 : 150,
+    // width: devicWidth < 380 ? 150 : 300,
+    // height: devicWidth < 380 ? 150 : 300,
+    // borderRadius: devicWidth < 380 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: "hidden",
